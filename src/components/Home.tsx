680f730c56d35897bcd84c62d9fa9463dd32e4d4
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2023-04-04 15:18:32 +0300

    socket

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,53 @@
 import { useEffect, useState } from "react"
-import {
-  Container,
-  Row,
-  Col,
-  Form,
-  FormControl,
-  ListGroup,
-  Button,
-} from "react-bootstrap"
+import { Container, Row, Col, Form, FormControl, ListGroup, Button } from "react-bootstrap"
+import { io } from "socket.io-client"
+import { Message, User } from "../types"
+
+const socket = io("http://localhost:3001", { transports: ["websocket"] })
 
 const Home = () => {
   const [username, setUsername] = useState("")
   const [message, setMessage] = useState("")
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([])
+  const [chatHistory, setChatHistory] = useState<Message[]>([])
+
+  useEffect(() => {
+    socket.on("welcome", (welcomeMessage) => {
+      console.log(welcomeMessage)
+
+      socket.on("loggedIn", (onlineUsersList) => {
+        console.log("logged in event:", onlineUsersList)
+        setLoggedIn(true)
+        setOnlineUsers(onlineUsersList)
+      })
+
+      socket.on("updateOnlineUsersList", (onlineUsersList) => {
+        console.log("A new user connected/disconnected")
+        setOnlineUsers(onlineUsersList)
+      })
+
+      socket.on("newMessage", (newMessage) => {
+        console.log(newMessage)
+        setChatHistory([...chatHistory, newMessage.message])
+      })
+    })
+  })
+
+  const submitUsername = () => {
+    // here we will be emitting a "setUsername" event (the server is already listening for that)
+    socket.emit("setUsername", { username })
+  }
+
+  const sendMessage = () => {
+    const newMessage: Message = {
+      sender: username,
+      text: message,
+      createdAt: new Date().toLocaleString("en-US"),
+    }
+    socket.emit("sendMessage", { message: newMessage })
+    setChatHistory([...chatHistory, newMessage])
+  }
 
   return (
     <Container fluid>
@@ -21,35 +57,41 @@ const Home = () => {
           {/* TOP AREA: USERNAME INPUT FIELD */}
           {/* {!loggedIn && ( */}
           <Form
-            onSubmit={e => {
+            onSubmit={(e) => {
               e.preventDefault()
+              submitUsername()
             }}
           >
-            <FormControl
-              placeholder="Set your username here"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
+            <FormControl placeholder="Set your username here" value={username} onChange={(e) => setUsername(e.target.value)} disabled={loggedIn} />
           </Form>
           {/* )} */}
           {/* MIDDLE AREA: CHAT HISTORY */}
-          <ListGroup></ListGroup>
+          <ListGroup>
+            {chatHistory.map((message, index) => (
+              <ListGroup.Item key={index}>
+                {<strong>{message.sender}</strong>} | {message.text} at {message.createdAt}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
           {/* BOTTOM AREA: NEW MESSAGE */}
           <Form
-            onSubmit={e => {
+            onSubmit={(e) => {
               e.preventDefault()
+              sendMessage()
             }}
           >
-            <FormControl
-              placeholder="Write your message here"
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            />
+            <FormControl placeholder="Write your message here" value={message} onChange={(e) => setMessage(e.target.value)} disabled={!loggedIn} />
           </Form>
         </Col>
         <Col md={3}>
           {/* ONLINE USERS SECTION */}
           <div className="mb-3">Connected users:</div>
+          {onlineUsers.length === 0 && <ListGroup.Item>Log in to check who is online!!</ListGroup.Item>}
+          <ListGroup>
+            {onlineUsers.map((user) => (
+              <ListGroup.Item key={user.socketId}>{user.username}</ListGroup.Item>
+            ))}
+          </ListGroup>
         </Col>
       </Row>
     </Container>
